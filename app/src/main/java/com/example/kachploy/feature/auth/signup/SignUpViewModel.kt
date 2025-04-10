6eb500398ba13fdbf9bfdd1fea7d6d0dbaa6d520
unknown <joshxchris098@gmail.com>
2025-04-10 21:36:18 +0100

    implemented sign up logic view model

--- app/src/main/java/com/example/kachploy/feature/auth/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/kachploy/feature/auth/signup/SignUpViewModel.kt
@@ -2,6 +2,7 @@ package com.example.kachploy.feature.auth.signup
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.auth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,17 +14,36 @@ class SignUpViewModel @Inject constructor(): ViewModel(){
     private val _state = MutableStateFlow<SignUpState>(SignUpState.Nothing)
     val state = _state.asStateFlow()
 
-    fun createUser(fullName: String, email: String, password: String){
+    fun createUser(firstName: String, lastName: String,email: String, password: String){
+        _state.value = SignUpState.Loading
         Firebase.auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if(task.isSuccessful){
-
+                    task.result.user?.let {
+                        it.updateProfile(UserProfileChangeRequest.Builder()
+                            .setDisplayName("$firstName $lastName")
+                            .build()).addOnCompleteListener {
+                                _state.value = SignUpState.Pending
+                                Firebase.auth.currentUser?.sendEmailVerification()
+                                    ?.addOnCompleteListener {
+                                        if(it.isSuccessful){
+                                            _state.value = SignUpState.Success
+                                        }else{
+                                            _state.value = SignUpState.Error
+                                        }
+                                    }
+                            }
+                        }
+                    _state.value = SignUpState.Error
+                }else{
+                    _state.value = SignUpState.Error
                 }
             }
     }
 }
 sealed class SignUpState{
     object Nothing: SignUpState()
+    object Pending: SignUpState()
     object Loading: SignUpState()
     object Success: SignUpState()
     object Error: SignUpState()
