04393d31fbacb0156bad9c6e1773ee70c21c76b4
unknown <joshxchris098@gmail.com>
2025-04-26 14:33:59 +0100

    created model class for user information

--- app/src/main/java/com/example/kachploy/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/kachploy/feature/auth/signin/SignInScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
