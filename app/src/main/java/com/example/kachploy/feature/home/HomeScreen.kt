fa6e95a93fa32478130ab117492431dac708e889
unknown <joshxchris098@gmail.com>
2025-05-08 14:20:16 +0100

    home page

--- app/src/main/java/com/example/kachploy/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/kachploy/feature/home/HomeScreen.kt
@@ -1,9 +1,67 @@
 package com.example.kachploy.feature.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.kachploy.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController){
+    val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MediumTopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.White,
+                titleContentColor = Color.Black
+            ),
+                title = {
+                    Text(text = "Home", maxLines = 1, overflow = TextOverflow.Ellipsis)
+                        },
+                navigationIcon = { IconButton(onClick = {}) {
+                    Image(painter = painterResource(id = R.drawable.google), contentDescription = "Profile")
+                } },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                })
+        }) {
+        Column(modifier = Modifier.fillMaxSize()
+            .padding(it)) {
+
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    HomeScreen(rememberNavController())
 }
